2599639b8f7d69bdb31c234f9b81d6f6a1f41f7f
d.ragozin <dmtryragozin@gmail.com>
2022-11-17 14:41:51 +0300

    [+] scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import * as React from 'react';
 
+import s from './App.module.scss';
+
 import foo from 'assets/hi/foo';
 import reactLogo from 'assets/react.svg';
 import { Bar } from 'components/assets/Bar';
-
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,18 +13,22 @@ function App() {
   foo(5);
 
   return (
-    <div className="App">
+    <div>
       <Bar />
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className={s.logo} alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className={`${s.logo} ${s.react}`}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={s.card}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -32,7 +36,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={s.readTheDocs}>
         Click on the Vite and React logos to learn more
       </p>
     </div>
