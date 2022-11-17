59daeeb81ffbda70ba6a32af5d9803973bce1761
d.ragozin <dmtryragozin@gmail.com>
2022-11-17 12:45:46 +0300

    [+] import linter config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
+
 import reactLogo from './assets/react.svg';
 import './App.css';
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 import './index.css';
-import * as React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
